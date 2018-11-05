857199775996e9f889f6e07cc393eb88a981603b
mr.liuyin1204 <1689450492@qq.com>
2018-11-05 09:52:23 +0800

    add 教学

---
+++ 教学/20181025/豆瓣最新热门电影/js/source.js
@@ -0,0 +1,15 @@
+// 热门
+var rmMovie = {name:"未择之路",score:"6.8",imgPath:"https://img3.doubanio.com/view/photo/s_ratio_poster/public/p2533833822.jpg"};
+var rmMovieArr = [rmMovie,rmMovie,rmMovie,rmMovie,rmMovie,rmMovie,rmMovie,rmMovie,rmMovie,rmMovie];
+
+// 最新
+var zxMovie = {name:"私人生活",score:"7.7",imgPath:"https://img3.doubanio.com/view/photo/s_ratio_poster/public/p2536784711.jpg"};
+var zxMovieArr = [zxMovie,zxMovie,zxMovie,zxMovie,zxMovie,zxMovie,zxMovie,zxMovie,zxMovie,zxMovie];
+
+// 豆瓣高分
+var dbgfMovie = {name:"红海行动",score:"8.3",imgPath:"https://img3.doubanio.com/view/photo/s_ratio_poster/public/p2514119443.jpg"};
+var dbgfMovieArr = [dbgfMovie,dbgfMovie,dbgfMovie,dbgfMovie,dbgfMovie,dbgfMovie,dbgfMovie,dbgfMovie,dbgfMovie,dbgfMovie];
+
+var movieArr = [{type:"热门",movies:rmMovieArr},
+				{type:"最新",movies:zxMovieArr},
+				{type:"豆瓣高分",movies:dbgfMovieArr}];
